ce4a0fe8589aeb586f4301d3bd9cbf5f2552a0eb
caiocesar333 <caiocesar333@outlook.com>
2023-06-11 00:47:11 -0300

    feat: adding frontend topLinks page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import Home from './layouts/Home.vue';
+// import Home from './layouts/Home.vue';
 // import Login from './layouts/Login.vue';
 // import SignUp from './layouts/SignUp.vue'
+import TopLinks from './layouts/TopLinks.vue';
 </script>
 
 <template>
- <Home/>
+ <TopLinks/>
 </template>
---
+++ frontend/src/layouts/TopLinks.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full text-center text-sm font-light bg-zinc-900">
+                <thead
+                  class="border rounded-xl bg-neutral-800 font-medium text-white dark:border-neutral-500 dark:bg-neutral-900">
+                  <tr>
+                    <th scope="col" class=" px-6 py-4">#</th>
+                    <th scope="col" class=" px-6 py-4">Short Link</th>
+                    <th scope="col" class=" px-6 py-4">Original Link</th>
+                    <th scope="col" class=" px-6 py-4">Total Clicks</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="border-b dark:border-neutral-500">
+                    <td class="whitespace-nowrap  px-6 py-4 font-medium">1</td>
+                    <td class="whitespace-nowrap  px-6 py-4">Mark</td>
+                    <td class="whitespace-nowrap  px-6 py-4">Otto</td>
+                    <td class="whitespace-nowrap  px-6 py-4">@mdo</td>
+                  </tr>
+                  <tr class="border-b dark:border-neutral-500">
+                    <td class="whitespace-nowrap  px-6 py-4 font-medium">2</td>
+                    <td class="whitespace-nowrap  px-6 py-4 ">Jacob</td>
+                    <td class="whitespace-nowrap  px-6 py-4">Thornton</td>
+                    <td class="whitespace-nowrap  px-6 py-4">@fat</td>
+                  </tr>
+                  <tr class="border-b dark:border-neutral-500">
+                    <td class="whitespace-nowrap  px-6 py-4 font-medium">3</td>
+                    <td class="whitespace-nowrap  px-6 py-4">Thornton</td>
+                    <td class="whitespace-nowrap  px-6 py-4">Mark</td>
+                    <td class="whitespace-nowrap  px-6 py-4">@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
