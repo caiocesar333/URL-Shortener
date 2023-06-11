008e97e261eddbac54a353286c9f4cf4923b939b
caiocesar333 <caiocesar333@outlook.com>
2023-06-11 01:31:26 -0300

    feat: adding login-to-signUp navigation router

--- frontend/src/layouts/Login.vue
+++ frontend/src/layouts/Login.vue
@@ -18,9 +18,11 @@
       <!-- Right column container -->
       <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
         <form>
-
           <!-- Email input -->
-          <div class="relative mb-6 bg-slate-800 rounded-xl" data-te-input-wrapper-init>
+          <div
+            class="relative mb-6 bg-slate-800 rounded-xl"
+            data-te-input-wrapper-init
+          >
             <input
               type="text"
               class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -34,7 +36,10 @@
           </div>
 
           <!-- Password input -->
-          <div class="relative mb-6 bg-slate-800 rounded-xl" data-te-input-wrapper-init>
+          <div
+            class="relative mb-6 bg-slate-800 rounded-xl"
+            data-te-input-wrapper-init
+          >
             <input
               type="password"
               class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -70,18 +75,7 @@
           <div class="text-center lg:text-right">
             <button
               type="button"
-              class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 
-              text-sm font-medium uppercase leading-normal text-white 
-              shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 
-              ease-in-out hover:bg-primary-600 
-              hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] 
-              focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] 
-              focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_
-              9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9p
-              x_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4p
-              x_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_
-              4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),
-              0_4px_18px_0_rgba(59,113,202,0.1)] bg-blue-700"
+              class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_ 9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9p x_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4p x_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_ 4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2), 0_4px_18px_0_rgba(59,113,202,0.1)] bg-blue-700"
             >
               Login
             </button>
@@ -90,6 +84,7 @@
             <p class="mb-0 mt-2 pt-1 text-sm font-semibold">
               Don't have an account?
               <a
+                @click="onClickRedirectTo"
                 href="#!"
                 class="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
                 >Register</a
@@ -101,3 +96,13 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const onClickRedirectTo = () => {
+  router.push("/signup");
+};
+</script>
