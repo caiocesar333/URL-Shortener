cb1db5e5179d199ff8836041dc9c10b35f09a8ed
caiocesar333 <caiocesar333@outlook.com>
2023-06-11 18:04:44 -0300

    feat: User home page linked to TopLinks and sort functionality debug

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="flex justify-between items-center m-16 bg-slate-900   border">
+  <div class="flex justify-between items-center m-16 bg-slate-900 border">
     <button
-      class="w-1/3 h-20 p-2 border-r-white rounded-none  hover:bg-blue-950 transition-all"
+      class="w-1/3 h-20 p-2 border-r-white rounded-none hover:bg-blue-950 transition-all"
     >
       See your Links
     </button>
     <button
-      class="w-1/3 h-20 p-2 border-r-white rounded-none   hover:bg-blue-950 transition-all"
+      @click="onClickRedirectTo"
+      class="w-1/3 h-20 p-2 border-r-white rounded-none hover:bg-blue-950 transition-all"
     >
       Top 100 Links
     </button>
     <button
-      class="w-1/3 h-20 p-2  rounded-none  hover:bg-blue-950 transition-all"
+      class="w-1/3 h-20 p-2 rounded-none hover:bg-blue-950 transition-all"
     >
       Loggout
     </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const onClickRedirectTo = () => {
+  router.push("/topLinks");
+};
+</script>
--- frontend/src/layouts/TopLinks.vue
+++ frontend/src/layouts/TopLinks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col">
-    <h1 class="font-bold text-7xl text-blue-600 mb-28">Top 100 Links! </h1>
+    <h1 class="font-bold text-7xl text-blue-600 mb-28">Top 100 Links!</h1>
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
         <div class="overflow-hidden">
@@ -21,14 +21,21 @@
                 :key="url._id"
                 class="border-b dark:border-neutral-500"
               >
-                <td class="whitespace-nowrap px-6 py-4 font-medium">{{ index + 1 }}</td>
+                <td class="whitespace-nowrap px-6 py-4 font-medium">
+                  {{ index + 1 }}
+                </td>
                 <td class="whitespace-nowrap px-6 py-4">{{ url.shortId }}</td>
                 <td class="px-6 py-4">
                   <div class="overflow-x-auto max-w-xs">
-                    <span class="whitespace-nowrap overflow-hidden overflow-ellipsis">{{ url.redirectURL }}</span>
+                    <span
+                      class="whitespace-nowrap overflow-hidden overflow-ellipsis"
+                      >{{ url.redirectURL }}</span
+                    >
                   </div>
                 </td>
-                <td class="whitespace-nowrap px-6 py-4">{{ url.visitHistory.length }}</td>
+                <td class="whitespace-nowrap px-6 py-4">
+                  {{ url.visitHistory.length }}
+                </td>
               </tr>
             </tbody>
           </table>
@@ -39,8 +46,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
 interface Url {
   _id: string;
@@ -53,14 +60,15 @@ const urls = ref<Array<Url>>([]);
 
 async function fetchUrls() {
   try {
-    const response = await axios.get('http://localhost:8001/url/top100');
-    console.log(response.data.urls)
-    urls.value = response.data.urls;
+    const response = await axios.get("http://localhost:8001/url/top100");
+    const sortedUrls = response.data.urls.sort(
+      (a: Url, b: Url) => b.visitHistory.length - a.visitHistory.length
+    );
+    urls.value = sortedUrls;
   } catch (error) {
     console.error(error);
   }
 }
-
 onMounted(() => {
   fetchUrls();
 });
