2eaaa24edb1ac92a27a3f60d975bd4d9d4a32d2c
caiocesar333 <caiocesar333@outlook.com>
2023-06-11 01:29:33 -0300

    feat: adding home-to-topLinks navigation router

--- frontend/src/components/Text.vue
+++ frontend/src/components/Text.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="flex flex-col gap-2">
     <h1 class="font-bold text-5xl text-cyan-500">Shorten Your Loooong Links</h1>
@@ -8,7 +6,17 @@
         ShortEN is an eficient and easy-to-use URL shortening service that
         streamlines your online experience.
       </p>
-      <p class="font-semibold">See the top links</p> <a class="hover:cursor-pointer text-cyan-500">here</a>
+      <p class="font-semibold">See the top links</p> <a @click="onClickRedirectTo" class="hover:cursor-pointer text-cyan-500">here</a>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const onClickRedirectTo = () => {
+  router.push("/topLinks");
+};
+</script>
