33c7019820ed203f452be7fe8fac8b86d52f300d
caiocesar333 <caiocesar333@outlook.com>
2023-06-11 22:29:29 -0300

    fix: adding some styles for login and signup

--- frontend/src/layouts/SignUp.vue
+++ frontend/src/layouts/SignUp.vue
@@ -1,68 +1,55 @@
-<template>
-  <div class="h-full">
-    <h1 class="font-bold text-7xl text-blue-600">Sign Up!</h1>
-    <!-- Left column container with background-->
-    <div
-      class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between"
-    >
-      <div
-        class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12"
-      >
-        <img
-          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          class="w-full"
-          alt="Sample image"
-        />
-      </div>
-
-      <!-- Right column container -->
-      <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-        <form @submit.prevent="handleSignUp">
-          <!-- userName input -->
-          <div
-            class="relative mb-6 bg-slate-800 rounded-xl"
-            data-te-input-wrapper-init
-          >
-            <input
-              v-model="userName"
-              type="text"
-              class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-50"
-              placeholder="Username"
-            />
-          </div>
-
-          <!-- Password input -->
-          <div
-            class="relative mb-6 bg-slate-800 rounded-xl"
-            data-te-input-wrapper-init
-          >
-            <input
-              v-model="password"
-              type="password"
-              class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-50"
-              placeholder="Password"
-            />
-          </div>
-
-          <!-- Error message -->
-          <div v-if="errorMessage" class="text-red-500 mb-4">
-            {{ errorMessage }}
-          </div>
-
-          <!-- Sign Up button -->
-          <div class="text-center lg:text-right">
-            <button
-              type="submit"
-              class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_ 9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9p x_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4p x_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_ 4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2), 0_4px_18px_0_rgba(59,113,202,0.1)] bg-blue-700"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
+<template class="h-screen">
+  <div class="h-full flex items-center justify-center">
+    <div class="w-full max-w-lg bg-slate-400 rounded-xl shadow-lg p-8">
+      <h1 class="text-4xl font-bold text-blue-600 mb-8">SignUp!</h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <img
+            src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="w-full rounded-xl"
+            alt="Sample image"
+          />
+        </div>
+        <div>
+          <form @submit.prevent="handleSignUp" class="space-y-6">
+            <div>
+              <label for="username" class="text-gray-900 font-semibold">Username</label>
+              <input
+                v-model="userName"
+                type="text"
+                id="username"
+                class="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:border-blue-600"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label for="password" class="text-gray-900 font-semibold">Password</label>
+              <input
+                v-model="password"
+                type="password"
+                id="password"
+                class="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:border-blue-600"
+                placeholder="Password"
+              />
+            </div>
+            <div v-if="errorMessage" class="text-red-500">
+              {{ errorMessage }}
+            </div>
+            <div>
+              <button
+                type="submit"
+                class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition duration-200 focus:outline-none focus:ring focus:border-blue-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref } from "vue";
